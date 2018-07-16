40d4fe0fe13cc2449521689b9c080db46411ae17
Andrii Kost <andriikost@Andriis-MacBook-Pro.local>
2018-07-16 18:19:37 -0500

    Auth and Authorization

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://andriikost:andriikost1@ds239681.mlab.com:39681/heroku_m1f8shqh',
+    database: 'mongodb://andrii:andriikost1@ds239681.mlab.com:39681/heroku_m1f8shqh',
     secret: 'yoursecret'
 };
